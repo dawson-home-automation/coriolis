c40df7c777571bc035b3fcaf2b93b2c43b069c3e
Dawson Israel <dawfre@amazon.com>
2018-08-22 10:46:41 -0300

    Clean up files

--- station/config/config.exs
+++ station/config/config.exs
@@ -1,17 +1,6 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# Customize non-Elixir parts of the firmware.  See
-# https://hexdocs.pm/nerves/advanced-configuration.html for details.
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
-
-# Use shoehorn to start the main application. See the shoehorn
-# docs for separating out critical OTP applications such as those
-# involved with firmware updates.
 config :shoehorn,
   init: [:nerves_runtime, :nerves_init_gadget],
   app: Mix.Project.config()[:app]
@@ -53,7 +42,6 @@ config :nerves_firmware_ssh,
     File.read!(Path.join(System.user_home!(), ".ssh/id_rsa.pub"))
   ]
 
-# Allows for tailing of logs.
 config :logger, backends: [RingLogger]
 config :logger, level: :debug
 
